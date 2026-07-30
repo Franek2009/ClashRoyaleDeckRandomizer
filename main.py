06ae888f3909cea8cbe88ce58fca8eeeb83eb7e8
Kontrabasista <Kontrabasista@proton.me>
2026-07-30 20:14:06 +0200

    Added card name extraction

--- main.py
+++ main.py
@@ -2,4 +2,15 @@ import json
 
 with open("cards.json", "r") as file:
     cards = json.load(file)
-print(cards[0])
+
+card_names = []
+#card_id = []
+
+for card in cards:
+    card_names.append(card['name'])
+#    card_id.append(card['id'])
+
+#print(cards[0])
+
+#print(card_names)
+#print(card_id)
