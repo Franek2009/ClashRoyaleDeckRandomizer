21c5aaec66040a0801c7b29cd83eda83376c1709
Kontrabasista <Kontrabasista@proton.me>
2026-07-30 20:44:23 +0200

    Added unavailable card filtering

--- main.py
+++ main.py
@@ -3,14 +3,34 @@ import json
 with open("cards.json", "r") as file:
     cards = json.load(file)
 
+with open("unavailable_cards.json", "r") as file:
+    unavailable_cards = json.load(file)
+
 card_names = []
-#card_id = []
+card_id = []
+unavailable_cards_id = []
+available_cards = []
+
+for unavailable_card in unavailable_cards:
+    unavailable_cards_id.append(unavailable_card['id'])
+
 
 for card in cards:
     card_names.append(card['name'])
-#    card_id.append(card['id'])
+    card_id.append(card['id'])
+    if card['id'] not in unavailable_cards_id:
+        available_cards.append(card)
+
+for card in available_cards:
+    if card['id'] in unavailable_cards_id:
+        print("BŁĄD:", card['name'])
+
+
+#print(len(cards))
+#print(len(available_cards))
 
 #print(cards[0])
-
+#print(unavailable_cards[0])
 #print(card_names)
 #print(card_id)
+#print(unavailable_cards_id)
