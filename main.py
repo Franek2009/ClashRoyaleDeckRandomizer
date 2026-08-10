ae21c929e16d6fec945dc8c3ca406a36900c8b55
Kontrabasista <Kontrabasista@proton.me>
2026-08-10 15:06:26 +0200

    Cleaned up code, numbered output, and added Terry to unavailable list

--- main.py
+++ main.py
@@ -7,8 +7,6 @@ with open("cards.json", "r") as file:
 with open("unavailable_cards.json", "r") as file:
     unavailable_cards = json.load(file)
 
-card_names = []
-card_id = []
 unavailable_cards_id = []
 available_cards = []
 
@@ -16,17 +14,15 @@ for unavailable_card in unavailable_cards:
     unavailable_cards_id.append(unavailable_card['id'])
 
 for card in cards:
-    card_names.append(card['name'])
-    card_id.append(card['id'])
     if card['id'] not in unavailable_cards_id:
         available_cards.append(card)
 
 deck = get_random_deck(available_cards)
-for card in deck:
-    print(card['name'])
+for number, card in enumerate(deck, 1):
+    print(number, card['name'])
+
 #print(len(cards))
 #print(len(available_cards))
-
 #print(cards[0])
 #print(unavailable_cards[0])
 #print(card_names)
