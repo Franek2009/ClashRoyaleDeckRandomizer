18d8fd38e50598f0225db9c45319d9325eb74ba0
Kontrabasista <Kontrabasista@proton.me>
2026-08-10 17:54:49 +0200

    Added a two-champion limit

--- randomizer.py
+++ randomizer.py
@@ -1,6 +1,7 @@
 import random
 
 def get_random_deck(loadCards):
+    champion_count = 0
     chosen_cards = []
     evolution_cards = []
     remaining_cards = []
@@ -10,9 +11,26 @@ def get_random_deck(loadCards):
             evolution_cards.append(card)
 
     chosen_cards.extend(random.sample(evolution_cards, 2))
+
+    for card in chosen_cards:
+        if card['rarity'] == 'champion':
+            champion_count += 1
+
     for card in loadCards:
         if card not in chosen_cards:
             remaining_cards.append(card)
 
-    chosen_cards.extend(random.sample(remaining_cards, 6))
+    while len(chosen_cards) < 8:
+        card = random.choice(remaining_cards)
+
+        if card['rarity'] == 'champion' and champion_count >= 2:
+            continue
+
+        chosen_cards.append(card)
+
+        if card['rarity'] == 'champion':
+            champion_count += 1
+
+        remaining_cards.remove(card)
+
     return chosen_cards
