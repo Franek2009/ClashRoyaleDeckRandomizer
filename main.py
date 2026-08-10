88bcec7aaa9b9ee233531fe545cd92fdb20bd6b8
Kontrabasista <Kontrabasista@proton.me>
2026-08-10 17:17:56 +0200

    Updated code for new card data

--- main.py
+++ main.py
@@ -4,28 +4,9 @@ from randomizer import get_random_deck
 with open("cards.json", "r") as file:
     cards = json.load(file)
 
-with open("unavailable_cards.json", "r") as file:
-    unavailable_cards = json.load(file)
+cards = cards['items']
 
-unavailable_cards_id = []
-available_cards = []
+deck = get_random_deck(cards)
 
-for unavailable_card in unavailable_cards:
-    unavailable_cards_id.append(unavailable_card['id'])
-
-for card in cards:
-    if card['id'] not in unavailable_cards_id:
-        available_cards.append(card)
-
-deck = get_random_deck(available_cards)
-for number, card in enumerate(deck, 1):
-    print(number, card['name'])
-
-#print(len(cards))
-#print(len(available_cards))
-#print(cards[0])
-#print(unavailable_cards[0])
-#print(card_names)
-#print(card_id)
-#print(unavailable_cards_id)
-#print(available_cards)
+for i, card in enumerate(deck, 1):
+    print(f"{i}. {card['name']}")
